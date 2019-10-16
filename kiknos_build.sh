99bd5c27f74cd426d588b98ac6bcff68bfb5ea80
Nikos <nikos.bre@gmail.com>
2019-10-16 14:23:10 -0700

    add dhcp plugin

--- kiknos_build.sh
+++ kiknos_build.sh
@@ -2,6 +2,6 @@
 ./autogen.sh &&
 ./configure --enable-socket-vpp --enable-libipsec --enable-kernel-vpp \
             --sysconfdir=/etc --with-piddir=/etc/ipsec.d/run \
-            --disable-kernel-netlink --disable-socket-default &&
+            --disable-kernel-netlink --disable-socket-default --enable-dhcp &&
 make &&
 sudo make install
