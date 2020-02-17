284aa4e97e6ac540bf677a1cb15462ff47d35216
Rastislav Szabo <raszabo@cisco.com>
2020-02-17 20:18:10 +0100

    install protobuf 3.11.2
    Signed-off-by: Rastislav Szabo <raszabo@cisco.com>

--- install_dep.sh
+++ install_dep.sh
@@ -14,7 +14,17 @@ git checkout v1.27.0
 git submodule update --init
 CFLAGS="-Wno-implicit-fallthrough -Wno-stringop-overflow -Wno-error=conversion" make
 sudo make install
+sudo ldconfig
 
+cd /tmp/
+git clone https://github.com/protocolbuffers/protobuf.git
+cd protobuf
+git checkout v3.11.2
+git submodule update --init --recursive
+./autogen.sh
+./configure
+make
+sudo make install
 sudo ldconfig
 
 cd ${WS}/api
